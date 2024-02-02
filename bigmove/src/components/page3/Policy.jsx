94220ea41fd48c2da30e0628cbea00de351887d0
kesle yrn <kesachyrn@gmail.com>
2024-02-02 15:54:55 +0200

    updated policy page

--- bigmove/src/components/page3/Policy.jsx
+++ bigmove/src/components/page3/Policy.jsx
@@ -11,31 +11,31 @@ function policy() {
      <p className='text'> 
       Our privacy policy underscores our dedication to safeguarding user information and ensuring the responsible use of data. We prioritize transparency, security, and compliance with privacy regulations to build trust with our stakeholders. <br /><br />
 
-1. Data Collection: <br />
+<b> 1. Data Collection: <br /></b>
 We collect minimal and necessary data to fulfill our mission, ensuring that the information is relevant to our environmental policy analysis efforts. This includes but is not limited to user preferences, device information, and interactions with our platform.<br /><br />
 
-2. Purpose of Data: <br />
+<b> 2. Purpose of Data: <br /></b>
 The data collected is solely used for improving our analytical capabilities, tailoring our services to user needs, and enhancing overall user experience. We do not share this information with third parties for commercial purposes.<br /><br />
 
-3. Data Security: <br />
+<b>3. Data Security: <br /></b>
 EnviroPolicy employs state-of-the-art security measures to protect user data from unauthorized access, disclosure, alteration, and destruction. Our infrastructure is designed to meet or exceed industry standards for data security.<br /><br />
 
-4. User Consent:<br />
+<b> 4. User Consent:<br /></b>
  By using our platform, users implicitly consent to the collection and processing of their data for the stated purposes. We respect user privacy and provide options for users to control and manage their data preferences.<br /><br />
 
-5. Data Retention:<br />
+<b> 5. Data Retention:<br /></b>
  We retain user data for the minimum necessary period required to achieve the purposes for which it was collected. Once data is no longer needed, it is securely and permanently deleted from our systems.<br /><br />
 
-6. Compliance: <br />
+<b> 6. Compliance: <br /></b>
 EnviroPolicy is committed to adhering to all relevant privacy laws and regulations. We regularly review and update our privacy practices to ensure ongoing compliance with the evolving legal landscape.<br /><br />
 
-7. Communication: <br />
+<b> 7. Communication: <br /></b>
 Users will be informed of any changes to our privacy policy through clear and accessible communication channels. We encourage users to stay informed about how their data is handled and to reach out with any privacy-related concerns.<br /><br />
 
-8. Accountability:<br />
+<b> 8. Accountability:<br /></b>
  We take responsibility for the protection of user data and continuously strive to uphold the highest standards of privacy and security in our environmental policy analysis initiatives.<br /><br />
 
-9. EnviroPolicy remains dedicated to promoting a sustainable future for Rwanda, and our privacy policy reflects our commitment to maintaining the trust and confidence of our users in this journey towards environmental heroism.<br /><br />
+ <b> EnviroPolicy remains dedicated to promoting a sustainable future for Rwanda,</b> and our privacy policy reflects our commitment to maintaining the trust and confidence of our users in this journey towards environmental heroism.<br /><br />
 
 
 </p>
